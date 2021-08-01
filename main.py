473265022055a52c63408c461cdc8d2b03072021
yotamh60 <yotampatos@gmail.com>
2021-08-01 06:33:48 +0300

    new branch

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # main py fille praciticing git 
 
 def Main(msg):
-    #adding second comment that will conflict the first oneg
-    # hey just putting a comment here for the merge
+    # no need for comments
     print(msg)
+
+def newfunc():
+    print("new func")
 
 Main("hello world")
 
